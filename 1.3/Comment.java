4002710ad0641d640c5ed9513ec96c1cef840c50
rusvlp <qdrwlad@gmail.com>
2021-09-24 20:38:47 +0300

    new realization of Comment

--- 1.3/Comment.java
+++ 1.3/Comment.java
@@ -1,15 +1,32 @@
 package com.company;
 
-public class Comment {
-    int rate;
-    String text;
+
+public class Comment{
+    public String text;
+    public int rate;
+    public Comment subcomm;
+    public int lvl;
 
     public Comment(int rate, String text){
         this.rate = rate;
         this.text = text;
+        this.lvl = 0;
     }
 
     public String toString(){
-        return "(" + rate + ")" + " " + text;
+        String lvlS = "";
+        for (int i = 0; i<this.lvl; i++)
+            lvlS += "---";
+
+        if (subcomm == null)
+            return lvlS + "(" + rate +")" + " " + text;
+        else{
+            subcomm.lvl = this.lvl + 1;
+            return lvlS + "(" + rate + ")" + text + "\n" + subcomm;
+        }
+
     }
+
 }
+
+
