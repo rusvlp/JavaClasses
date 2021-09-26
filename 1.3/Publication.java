7d7323710d9b8a367538d2db68c9512290b04b89
rusvlp <qdrwlad@gmail.com>
2021-09-26 08:48:09 +0400

    Publication builder

--- 1.3/Publication.java
+++ 1.3/Publication.java
@@ -1,4 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Publication {
+    public String name;
+    public int rate;
+    public String text;
+    public List<Comment> comments = new ArrayList<>();
+    public List<String> tags = new ArrayList<>();
+
+    
 }
---
+++ 1.3/PublicationBuilder.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface PublicationBuilder {
+    public void setName();
+
+    public void setRate();
+
+    public void
+}
